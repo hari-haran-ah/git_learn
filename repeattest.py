27d346666533ccf88bde8619306fab9c75ed219b
hari-haran-ah <hariharan8351@gmail.com>
2025-09-13 15:12:22 +0530

    count substring

--- repeattest.py
+++ repeattest.py
@@ -1,33 +1,32 @@
+# product of three number
+def sum_product(nums,target):
+    n = len(nums)
+    result =[]
+    count = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                if nums[i] * nums[j] * nums[k] == target:
+                    result.append((nums[i],nums[j],nums[k]))
+                    count +=1
+    return result,count
+nums = list(map(int,input("Enter the Value:").split()))
+target =int( input("Enter the target Number: "))
+result,count = sum_product(nums,target)
+print(f" combination are :{result}")
+print(f"the count of combination :{count}")
 
-    
-#file char check
-def check_word(filename):
-    with open(filename, "r") as f:
-        words = f.read().split()
-        freq = {}
-        for word in words:
-            if word in freq:
-                freq[word] += 1
-            else:
-                freq[word] = 1
-        for word in words:
-            if freq[word] == 1:
-                return word
-        return None
-filename = input("Enter the file Name:")
-result = check_word(filename)
-print(result)
-
-
-# word len each row
-def word_len_each_row(filename):
-    with open(filename , "r") as f:
-        line_no = 0
-        for line in f:
-            words = line.split()
-            print(f"{words}Line num{line_no} ->{len(words)}")
-            line_no +=1
-
-filename = input("Enter the file name:")
-word_len_each_row(filename)
-            
+# count substring
+def count_sub(string,target):
+    total = []
+    count = 0
+    for i in range(len(string) - len(target) + 1):
+        if string[i:i+len(target)] == target:
+            total.append(string[i:i+len(target)])
+            count += 1
+    return total ,count
+string = input("Enter the String:")
+target = input("Enter the Substring:")
+result,count = count_sub(string,target)
+print(f"the string are :{result}")
+print(f"count are :{count}")
--- testing.py
+++ testing.py
@@ -848,3 +848,35 @@ for i in range(10):       # 360 small steps
 turtle.done()
 
 
+# product of three number
+def sum_product(nums,target):
+    n = len(nums)
+    result =[]
+    count = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                if nums[i] * nums[j] * nums[k] == target:
+                    result.append((nums[i],nums[j],nums[k]))
+                    count +=1
+    return result,count
+nums = list(map(int,input("Enter the Value:").split()))
+target =int( input("Enter the target Number: "))
+result,count = sum_product(nums,target)
+print(f" combination are :{result}")
+print(f"the count of combination :{count}")
+
+# count substring
+def count_sub(string,target):
+    total = []
+    count = 0
+    for i in range(len(string) - len(target) + 1):
+        if string[i:i+len(target)] == target:
+            total.append(string[i:i+len(target)])
+            count += 1
+    return total ,count
+string = input("Enter the String:")
+target = input("Enter the Substring:")
+result,count = count_sub(string,target)
+print(f"the string are :{result}")
+print(f"count are :{count}")
